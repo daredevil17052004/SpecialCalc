385e16cbc9bd3d418b1d68a5f5745e79f43e8ad0
daredevil17052004 <anshs052004@gmail.com>
2023-10-18 15:25:23 +0530

    did changes in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,45 @@
 // Progression 1: Create special addition
 
+function specialAddition(numberOne, numberTwo){
+    
+    if(numberOne<0){
+        return `Negative numbers are not allowed`
+    }
+
+    if(numberTwo<0){
+        return `Negative numbers are not allowed`
+    }
+
+    return numberOne - numberTwo;
+}
+
 // function specialAddition(){}
 
 // Progression 2: Create simple division
 
+function simpleDivision(numberOne,numberTwo){
+
+    if(numberTwo>numberOne){
+        return `Cannot divide a smaller number from a larger number`
+    }
+
+    if(numberOne<0){
+        return `Negative numbers are not allowed`
+    }
+
+    if(numberTwo<0){
+        return `Negative numbers are not allowed`
+    }
+
+    if(numberTwo===0){
+        return `Not a number`
+    }
+
+    return numberOne/numberTwo;
+}
+
 // Progression 3: Create the special calculator
+
+function specialCalculator(numberOne,numberTwo,operation){
+    return operation(numberOne,numberTwo);
+}
